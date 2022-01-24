72a2b3d6988167e7fa7c50a3ad602b29f3100403
aa980 <78963730+aa980@users.noreply.github.com>
2022-01-24 18:57:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function switchon(){
+    document.getElementById('imglighton').src='Light-Bulb-DRAWING-–-STEP-10.jpeg'
+} 
+
+function switchoff(){
+    document.getElementById('imglighton').src='lightbulbon.jpeg'
+} 
